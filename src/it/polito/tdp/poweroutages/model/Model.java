939e4d77cb4693614c9aa2bceb4d3b2817cea477
Serena <Serena@HP.station>
2019-05-13 22:26:38 +0200

    Laboratorio completato

--- src/it/polito/tdp/poweroutages/model/Model.java
+++ src/it/polito/tdp/poweroutages/model/Model.java
@@ -20,6 +20,7 @@ public class Model {
 	PowerOutageDAO podao;
 	List<Nerc> nerc;
 	List<PowOutages> res;
+	int maxPeopleAffected;
 
 	public Model() {
 		podao = new PowerOutageDAO();
@@ -34,23 +35,24 @@ public class Model {
 		List<PowOutages> poList = podao.getPowOutages(nerc.getId()); // prendo tutti i blackout avvenuti in quel nerc
 		List<PowOutages> parziale = new ArrayList<>();
 		this.res = new ArrayList<PowOutages>();
-		cerca(parziale, poList, years, hours, 0);
+		maxPeopleAffected = 0;
+		cerca(parziale, poList, years, hours);
 		return res;
 	}
 
-	public void cerca(List<PowOutages> parziale, List<PowOutages> poList, int years, int hours, int L) {
+	/*public void cerca(List<PowOutages> parziale, List<PowOutages> poList, int years, int hours, int L) {
 
 		// casi terminali
 
 		if (L == poList.size()) {
 			return;
 		}
-		if (calcolaAnno(parziale) <= years && calcolaOre(parziale) <= hours) {
-
-			this.res = new ArrayList<PowOutages>(parziale);
-			System.out.println("Finito " + res.toString());
-			return;
+		if(calcolaAnno(parziale) <= years && calcolaOre(parziale) <= hours) {
+				System.out.println(parziale.toString() + "\n");
+				this.res = new ArrayList<PowOutages>(parziale);
+				return;
 		}
+
 
 		// genero sotto-problemi
 		// blackout[L] è da aggiungere?
@@ -61,21 +63,48 @@ public class Model {
 		parziale.add(poList.get(L));
 		cerca(parziale, poList, years, hours, L + 1);
 		parziale.remove(poList.get(L));
+		
+	}*/
+	public void cerca(List<PowOutages> parziale, List<PowOutages> poList, int years, int hours) {
+
+		// casi terminali
+		if(sumMaxPeopleAffected(parziale) > this.maxPeopleAffected) {
+			this.maxPeopleAffected = sumMaxPeopleAffected(parziale);
+			this.res = new ArrayList<PowOutages>(parziale);
+		}
+
+		// lo aggiungo
+		for(PowOutages po : poList) {
+			if(!parziale.contains(po)) {
+				parziale.add(po);
+				if(calcolaAnno(parziale) <= years && calcolaOre(parziale) <= hours) {
+					cerca(parziale, poList, years, hours);
+				}
+				parziale.remove(po);
+			}
+		}	
 	}
+	
+	public int sumMaxPeopleAffected(List<PowOutages> poList) {
+		int sum = 0;
+		for (PowOutages po : poList) {
+			sum += po.getCustomer();
+		}
+		return sum;
+	}
+
+	public int calcolaOre(List<PowOutages> poList) {
+		int sum = 0;
+		for (PowOutages po : poList) {
+ 
+			sum += po.getDateStart().until(po.getDateEnd(), ChronoUnit.HOURS);
+		}
+		return sum;
+	}
+	
 
 	public int calcolaAnno(List<PowOutages> poList) {
 
-		/*
-		 * Date recentEnd = poList.get(0).getDateEnd(); //for(PowOutages po : poList) {
-		 * //if(po.getDateEnd().after(recentEnd)) { //recentEnd = po.getDateEnd(); //}
-		 * //} //Date oldStart = poList.get(0).getDateStart(); for(PowOutages po :
-		 * poList) { if(po.getDateStart().before(oldStart)) { oldStart =
-		 * po.getDateStart(); } } Calendar calEnd = Calendar.getInstance();
-		 * calEnd.setTime(recentEnd); Calendar calStart = Calendar.getInstance();
-		 * calStart.setTime(oldStart); return
-		 * java.lang.Math.abs(calEnd.get(Calendar.YEAR)-calStart.get(Calendar.YEAR));
-		 */
-		if (poList.size() >= 2) {
 			LocalDate recentEnd = poList.get(0).getDateEnd().toLocalDate();
 			LocalDate oldStart = poList.get(0).getDateStart().toLocalDate();
 
@@ -89,36 +118,6 @@ public class Model {
 				}
 			}
 			return recentEnd.getYear() - oldStart.getYear();
-		} else
-			return 10000;
-	}
 
-	public int calcolaOre(List<PowOutages> poList) {
-		int sum = 0;
-		for (PowOutages po : poList) {
-			/*
-			 * Calendar calEnd = Calendar.getInstance(); calEnd.setTime(po.getDateEnd());
-			 * Calendar calStart = Calendar.getInstance();
-			 * calStart.setTime(po.getDateStart());
-			 * 
-			 * sum = sum + java.lang.Math.abs((calEnd.get(Calendar.MILLISECOND) -
-			 * calStart.get(Calendar.time))); System.out.println(sum);
-			 */
-
-			// sum = sum +
-			// java.lang.Math.abs(po.getDateEnd().toLocalTime().until(po.getDateStart().toLocalTime(),
-			// hours));
-			LocalDateTime tempDateTime = LocalDateTime.from(po.getDateStart());
-			System.out.println(tempDateTime);
-			System.out.println(po.getDateEnd());
-			sum += tempDateTime.until(po.getDateEnd(), ChronoUnit.HOURS);
-			/*
-			 * LocalTime l1 = po.getDateEnd().toLocalTime(); LocalTime l2 =
-			 * po.getDateStart().toLocalTime(); sum = sum + Duration.between(l1,
-			 * l2).toNanos();
-			 */
-			System.out.println(sum);
-		}
-		return sum;
 	}
 }
